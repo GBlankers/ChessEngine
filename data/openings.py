a7ceb30063d21c04d390ff1bc144cf9d23e9996e
Muntan <37872680+TheMuntan@users.noreply.github.com>
2020-11-17 15:56:20 +0100

    Update openings.py

--- data/openings.py
+++ data/openings.py
@@ -2,4 +2,4 @@
 openings = [["e2e4", "g1f3", "f1c4"], ["e2e4", "g1f3", "f1b5"], ["d2d4", "c2c4"], ["e2e4", "d2d4"], ["b2b3"],
             ["g1f3", "g2g3"], ["d2d4", "c2c4", "g2g3"]]
 # 1. Italian Game, 2. Ruy Lopez, 3. Queen's Gambit, 4. Pirc Defence, 5. Nimzowitsch-Larsen Attack,
-# 6. King's Indian Attack , 7.
+# 6. King's Indian Attack , 7. Catalan
